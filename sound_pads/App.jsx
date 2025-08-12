e6c61d4e6fcb6e942b084647712fde1be973bfa8
Venkat11N <vnaiduari@gmail.com>
2025-08-12 16:25:32 +0530

    [ Sound pads challenge ]

--- sound_pads/App.jsx
+++ sound_pads/App.jsx
@@ -1,8 +1,23 @@
 import React from "react"
 import padsData from "./pads"
+import Pad from "./Pad"
 
 export default function App() {
     const [pads, setPads] = React.useState(padsData); 
+
+    const buttonElements = pads.map(pad => (
+        <Pad key={pad.id} color={pad.color} on={pad.on} />
+    ))
+
+        /**
+     * Challenge: use a ternary to determine the backgroundColor
+     * of the buttons
+     * If darkMode is true, set them to "#222222"
+     * If darkMode is false, set them to "#cccccc"
+     */
+
+ 
+
     /**
      * Challenge part 1:
      * 1. Initialize state with the default value of the
@@ -12,13 +27,24 @@ export default function App() {
      *    (Don't worry about using the "on" or "color" 
      *    properties yet)
      */
+    // const styles = {
+    //     backgroundColor: darkMode ? "#222222" : "#cccccc",
+    // }
+
+    // const buttonElements = pads.map((pad, index) => (
+    //     <button                            style={{
+    //             backgroundColor: darkMode ? 
+    //                 (index % 2 === 0 ? "#333333" : "#222222") 
+    //                 : (index % 2 === 0 ? "#aaaaaa" : "#cccccc")
+    //         }} 
+    //         key={pad.id}
+    //     >
+
+    //     </button>
+
+    // ))
 
 
-    const buttonElements = pads.map(pad => (
-        <button key={pad.id}>
-
-        </button>
-    ))
     return (
         <main>
             <div className="pad-container">
